b7de309a5c8ed74173f303d8f64f1d54a351f7ec
Faizu <faizu42564@gmail.com>
2025-05-30 21:44:01 +0530

    Update server.py to bind host 0.0.0.0 and dynamic port for Render deployment

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+import os  # add this
 
 app = Flask(__name__)
 
@@ -7,5 +8,5 @@ def serve_index():
     return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    # Change this line:
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
